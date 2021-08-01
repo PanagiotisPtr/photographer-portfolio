a571175ed63bb953c41bba1702ba26bea83152d3
PanagiotisPtr <lpanagiotispetridisl@gmail.com>
2021-08-01 14:39:31 +0300

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Slab" />
+      </Head>
+      <body>
+        <h1>Hello</h1>
+      </body>
+    </div>
+  )
+}
